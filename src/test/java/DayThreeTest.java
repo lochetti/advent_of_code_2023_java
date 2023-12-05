afd8107e300a18dbb25310c76ca8618accaecdab
Renato Lochetti <renato.lochetti@gmail.com>
2023-12-05 15:34:46 +0000

    Day 3 - Adding more tests

--- src/test/java/DayThreeTest.java
+++ src/test/java/DayThreeTest.java
@@ -13,6 +13,48 @@ public class DayThreeTest {
     }
 
     @Test
+    void testFirstExample2() throws IOException {
+        int result = DayThree.first("day_three_first_example2");
+
+        Assert.assertEquals(result, 156);
+    }
+
+    @Test
+    void testFirstExample3() throws IOException {
+        int result = DayThree.first("day_three_first_example3");
+
+        Assert.assertEquals(result, 1000);
+    }
+
+    @Test
+    void testFirstExample4() throws IOException {
+        int result = DayThree.first("day_three_first_example4");
+
+        Assert.assertEquals(result, 10);
+    }
+
+    @Test
+    void testFirstExample5() throws IOException {
+        int result = DayThree.first("day_three_first_example5");
+
+        Assert.assertEquals(result, 21);
+    }
+
+    @Test
+    void testFirstExample6() throws IOException {
+        int result = DayThree.first("day_three_first_example6");
+
+        Assert.assertEquals(result, 676);
+    }
+
+    @Test
+    void testFirstExample7() throws IOException {
+        int result = DayThree.first("day_three_first_example7");
+
+        Assert.assertEquals(result, 301);
+    }
+
+    @Test
     void testFirstReal() throws IOException {
         int result = DayThree.first("day_three_first_real");
 
